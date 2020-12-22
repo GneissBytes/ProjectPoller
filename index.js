7cce7b7f62f85241fc8ad6efbd1bd8ce217b843a
GneissBytes <59232985+GneissBytes@users.noreply.github.com>
2020-12-22 14:41:28 +0100

    deployment config
    heroku-postbuild script

--- index.js
+++ index.js
@@ -26,9 +26,21 @@ app.use('/api', apiRoutes)
 app.use('/api', billingRoutes)
 app.use('/auth', authRoutes)
 
-app.get('*', (req, res) => {
-    res.send('<h1>404 not found</h1>')
-});
+
+
+if (process.env.NODE_ENV == 'production') {
+    // Express will send static production assets, ie main.js main.css
+    app.use(express.static('client/build'))
+    // Express will send index.html if route is unknown
+    const path = require('path');
+    app.get('*', (_req, res) => {
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.hmtl'))
+    })
+} else {
+    app.get('*', (_req, res) => {
+        res.send('<h1>404 not found</h1>')
+    });
+}
 
 port = process.env.PORT || 5000
 app.listen(port, () => {

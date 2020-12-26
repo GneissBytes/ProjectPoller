8a2fea9885a133f491dd569be064b99042ca0b34
GneissBytes <59232985+GneissBytes@users.noreply.github.com>
2020-12-26 16:32:17 +0100

    plot

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { fetchSurveys } from '../../actions'
+import { PieChart } from 'react-minimal-pie-chart';
 
 class SurveyList extends Component {
     componentDidMount() {
@@ -12,10 +13,14 @@ class SurveyList extends Component {
             return (
                 <div className="card light-blue lighten-5" key={survey._id}>
                     <div className="card-content">
-                        <span className="card-title">{survey.title}</span>
+                        <span className="card-title">
+                            {survey.title}
+                        </span>
                         <p>
                             {survey.body}
+
                         </p>
+
                         <p className="right">
                             Published: {new Date(survey.dateSent).toLocaleDateString()}
                         </p>
@@ -23,6 +28,16 @@ class SurveyList extends Component {
                     <div className="card-action">
                         <a>Yes: {survey.yes}</a>
                         <a>No: {survey.no}</a>
+                        <span className="right" style={{display: "inline-block"}}>
+                        <PieChart style={{ height: "20px", display: "inline", padding:0, margin:0 }}
+                            id={`pie${survey.title}`}
+                            className="left"
+                            data={[
+                                { title: 'Yes', value: survey.yes, color: 'blue' },
+                                { title: 'No', value: survey.no, color: 'red' }
+                            ]}
+                            viewBoxSize={[100, 100]} />
+                        </span>
                     </div>
                 </div>
             )
